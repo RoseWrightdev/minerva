9deb5b08d21cadf75822548927c3079f6e5568e5
RoseWrightdev <rosesophiewright@gmail.com>
2025-03-17 21:09:28 -0500

    bash init

---
+++ scripts/run.sh
@@ -0,0 +1,10 @@
+cd minerva
+
+# networking
+kubectl apply -f gateway/gateway-api.yaml
+
+# minerva
+kubectl apply -f minerva/minerva-namespace.yaml
+kubectl apply -f minerva/minerva-deployment.yaml
+kubectl apply -f minerva/minerva-service.yaml
+kubectl apply -f minerva/minerva-httproute.yaml
